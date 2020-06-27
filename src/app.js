172d194021f6c766384d91db483a7926539d88e8
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-06-27 18:33:12 -0400

    fixed syntax errors

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const uuid = require('uuid/v4')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const uuid = require('uuid/v4');
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
-
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(bookmarsRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello, world')
-})
+    res.send('Hello, world');
+});
 
 
-module.exports = app
+module.exports = app;
 
