2d8109dea2ec50027c095e962a6ddd286b86e1c9
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-07-13 22:34:47 -0400

    removed uncessary comments

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -10,12 +10,6 @@ const bookmarksRouter = express.Router()
 const bodyParser = express.json()
 
 
-// Write a route handler for the endpoint GET /bookmarks that returns a list of bookmarks
-// Write a route handler for the endpoint GET /bookmarks/:id that returns a single bookmark with the given ID, return 404 Not Found if the ID is not valid
-// Write a route handler for POST /bookmarks that accepts a JSON object representing a bookmark and adds it to the list of bookmarks after validation.
-// Write a route handler for the endpoint DELETE /bookmarks/:id that deletes the bookmark with the given ID.
-
-
 bookmarksRouter
 .route('/bookmarks') //use with id to get bookmark route using id
 .get((req, res) => {
